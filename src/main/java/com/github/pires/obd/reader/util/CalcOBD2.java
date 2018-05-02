95a0a9489a77d40a1c8244cda4473e17d53ff5df
mathiaseloi <mathias.eloi@gmail.com>
2018-05-02 15:24:39 -0300

    Adicionado novos calculos para chegar o IMAF para veículos que não possuem o sensor;

--- src/main/java/com/github/pires/obd/reader/util/CalcOBD2.java
+++ src/main/java/com/github/pires/obd/reader/util/CalcOBD2.java
@@ -1,29 +1,77 @@
 package com.github.pires.obd.reader.util;
 
 
+/**
+ *
+ */
 public class CalcOBD2 {
 
-    //private final int SECONDS_HOUR = 3600;
-
-    public CalcOBD2() {
-
-    }
-
+    /**
+     * @param fuel
+     * @param massAirFlow
+     * @param vehicleSpeed
+     * @return
+     */
     public static double getFuelConsumption(Fuel fuel, double massAirFlow, int vehicleSpeed) {
         return (fuel.getAirFuel() * fuel.getDensityFuel() * vehicleSpeed) / (3600 * massAirFlow);
         // measured in km/l
     }
 
+    /**
+     * @param rpm
+     * @param map
+     * @param iat
+     * @return
+     */
+    public static double getIMAP(int rpm, double map, double iat) {
+        return ((rpm * map) / iat) / 2;
+    }
+
+    /**
+     * @param imap
+     * @param vdm
+     * @return
+     */
+    public static double getMAF(double imap, double vdm) {
+
+        // average molecular mass of air
+        double mm = 28.97;
+
+        // volumetric efficiency in percent
+        int ev = 85;
+
+        // constant kelvin joules per mole
+        double r = 8.314;
+
+
+        return ((imap / 60) * ev * vdm * mm) / r;
+    }
+
+    /**
+     * @param totalCapacity
+     * @param fuelInputPercent
+     * @return
+     */
     public double getFuelCapacity(double totalCapacity, double fuelInputPercent) {
         return totalCapacity * (fuelInputPercent / 100);
         // measured in liters
     }
 
+    /**
+     *
+     * @param consumption
+     * @param totalCapacity
+     * @param fuelInputPercent
+     * @return
+     */
     public double getFuelCapacityRange(double consumption, double totalCapacity, double fuelInputPercent) {
         return getFuelCapacity(totalCapacity, fuelInputPercent) * consumption;
         // autonomy in km range
     }
 
+    /**
+     *
+     */
     public enum Fuel {
 
         Diesel(14.5, 800),
@@ -51,11 +99,4 @@ public class CalcOBD2 {
             return densityFuel;
         }
     }
-
-    // calc velocidade media
-    // calc tempo decorrido
-    // calc consumo medio
-
-    // criar uma TRIP com o VIN
-    // registrar na memoria os dados velocidade, tempo, consumo quando o motor estiver ligado.
 }
